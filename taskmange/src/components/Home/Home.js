240d65fa692bb55f0f83944d12e6cef81a8a9784
rahadislam <rahadislm.web@gmil.com>
2022-06-30 17:50:20 +0600

    add task from implement

--- taskmange/src/components/Home/Home.js
+++ taskmange/src/components/Home/Home.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Home = () => {
+    const { register, formState: { errors }, handleSubmit ,reset} = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    }
     return (
-        <div>
-            <h1>ok</h1>
+            <div className='w-96 mx-auto'>
+            <h1 className='text-center font-bold text-2xl'>Add Task</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Task Title</span>
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Task Title"
+                        className="input input-bordered w-full max-w-xs"
+                        {...register("name")}
+                        required
+                    />
+                </div>
+
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Description</span>
+                    </label>
+                    <textarea
+                        type="text"
+                        placeholder="Your description"
+                        className="input input-bordered w-full max-w-xs"
+                        {...register("description")} required
+                    />
+                </div>
+                            <br />
+                <input className='btn btn-primary  text-white' type="submit" value="ADD NOW" />
+            </form>
+
         </div>
     );
 };
